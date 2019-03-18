09a12b81124f435a4d9d4b2306cd79519c35642e
Scott Dallinga <sdallinga@gmail.com>
2019-03-18 15:54:35 -0400

    End of Monday

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CommunicationsComponent } from './features/communications/communications.component';
+
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'communications',
+        component: CommunicationsComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
