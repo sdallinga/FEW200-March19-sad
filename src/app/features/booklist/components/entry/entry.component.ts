6a7ead638ce0905ff69fb6ddd8b5695681688dfb
Scott Dallinga <sdallinga@gmail.com>
2019-03-25 16:39:41 -0400

    Implemented Book Entry template

--- src/app/features/booklist/components/entry/entry.component.ts
+++ src/app/features/booklist/components/entry/entry.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from '../../reducers';
+import { AddedBook } from '../../actions/list.actions';
 
 @Component({
-  selector: 'app-entry',
-  templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.css']
+    selector: 'app-entry',
+    templateUrl: './entry.component.html',
+    styleUrls: ['./entry.component.css']
 })
 export class EntryComponent implements OnInit {
 
-  constructor() { }
+    constructor(private store: Store<State>) { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
+    /*
+        add(item: HTMLInputElement) {
+            // dispatch and action
+            this.store.dispatch(new AddedBook(item.value));
+            item.value = '';
+            item.focus();
+        }
+    */
 }
